63fac4a594324d150f7ccd059d2471137481d915
Maxim Sigalov <xvs@google.com>
2019-11-29 23:11:56 -0500

    whatever

--- codenames.py
+++ codenames.py
@@ -1,7 +1,16 @@
 from gensim import models
+import gensim
+from nltk import find
+from nltk.corpus import brown
+from nltk import FreqDist
 from random import randint
 from itertools import combinations
+import more_itertools
 import math
+from math import inf
+import numpy as np
+from numpy import dot
+from numpy.linalg import norm
 
 # GAME DEFINITION
 NUM_BOARD_WORDS = 25
@@ -15,10 +24,10 @@ BOARD = {}
 MODEL = None
 
 # ALGO 1 VARIABLES
-GOOD_COEF = 4
+GOOD_COEF = 10
 MEH_COEF = -1
-BAD_COEF = -3
-WORST_COEF = -5
+BAD_COEF = -5
+WORST_COEF = -10
 RELEVANCE_THRESHOLD_LOW = 0.32
 RELEVANCE_THRESHOLD_HIGH = 0.95
 
@@ -30,9 +39,9 @@ SIM_FACTOR = (9/10)
 def loadWords():
     wordsfile = open('words.txt', 'r')
     for line in wordsfile:
-        word = line.strip()
+        word = line.strip().lower()
         if word in MODEL.vocab.keys():
-            WORDS.add(word)    
+            WORDS.add(word)
 
 def getRandomWord(usedWords):
     diff = WORDS.difference(usedWords)
@@ -65,46 +74,103 @@ def getAllWords():
     for k in BOARD:
         words = words.union(BOARD[k])
     return words
-        
+
 def loadModel(lim):
     global MODEL
-    MODEL = models.KeyedVectors.load('best_500.bin', mmap='r')
+    #MODEL = models.KeyedVectors.load('best_500.bin', mmap='r')
+    #word2vec_sample = str(find('models/word2vec_sample/pruned.word2vec.txt'))
+    #MODEL = gensim.models.KeyedVectors.load_word2vec_format(word2vec_sample, binary=False)
+    #MODEL = gensim.models.KeyedVectors.load('brownlow.txt', mmap='r')
+    MODEL = gensim.models.KeyedVectors.load('gnews100000.txt', mmap='r')
 
 def getWordSimilarity(word1, word2):
     return MODEL.similarity(word1, word2)
 
+def cos_sim(a, b):
+    return dot(a,b) / (norm(a) * norm(b))
+
+def score(wset, vectors, mehWords, badWords, worstWords):
+    center_of_mass = sum(vectors[w] for w in wset)
+    center_of_mass /= norm(center_of_mass)
+
+    score = 0
+    for w in wset:
+      score += cos_sim(vectors[w], center_of_mass) * GOOD_COEF
+
+    for w in mehWords:
+      score += cos_sim(vectors[w], center_of_mass) * MEH_COEF * len(wset)
+
+    for w in badWords:
+      score += cos_sim(vectors[w], center_of_mass) * BAD_COEF * len(wset)
+
+    for w in worstWords:
+      score += cos_sim(vectors[w], center_of_mass) * WORST_COEF * len(wset)
+
+    return score, center_of_mass
+
+def score_partition(partition, vectors, mehWords, badWords, worstWords):
+    wset_to_sc_com = {}
+    for wset in partition:
+      wset_to_sc_com[tuple(wset)] = score(wset, vectors, mehWords, badWords, worstWords)
+
+    total_score = 1.01**(((1/len(wset_to_sc_com))**1.2) * (sum(wset_to_sc_com[wset][0] * len(wset) for wset in wset_to_sc_com)))
+    return total_score, wset_to_sc_com
+
+def find_closest(com, candidate_words, vectors, allWords):
+    return max({cw for cw in candidate_words if cw[0] not in allWords and
+                all((cw[0] not in word) and (word not in cw[0]) for word in allWords)},
+               key=lambda word_freq: cos_sim(com, vectors[word_freq[0]]))[0]
+
 def getMove(team):
     if MODEL is None:
         return None
+
+    dist = FreqDist(w.lower() for w in brown.words())
+    allWords = getAllWords()
+
+    candidate_words = dist.most_common(50000)
     goodWords = BOARD[team]
+    #candidate_words = {cw for cw in candidate_words if (cw[0] in MODEL.vocab.keys())}
+    candidate_words = {(word, 0) for word in MODEL.vocab.keys() if '_' not in word}
     mehWords = BOARD['tan']
     badWords = set()
     worstWords = BOARD['black']
+
+    vectors = {word[0] : MODEL.get_vector(word[0]) for word in candidate_words}
+    #print('vectorslen!!!!', len(vectors))
+
     for k in BOARD:
         if not k in [team, 'tan', 'black']:
             badWords = badWords.union(BOARD[k])
 
-    equation = []
-    for word in goodWords:
-        equation.append([GOOD_COEF, word])
-    for word in mehWords:
-        equation.append([MEH_COEF, word])
-    for word in badWords:
-        equation.append([BAD_COEF, word])
-    for word in worstWords:
-        equation.append([WORST_COEF, word])
+    partitions = more_itertools.set_partitions(goodWords)
+    best_score = -float(inf)
+    best_partition = {}
+    partition_sizes = np.zeros(9)
+    partition_scores = np.zeros(9)
+    partition_scores_max = np.ones(9) * -float(inf)
+    for partition in partitions:
+        sc, partition_to_sc_com = score_partition(partition, vectors, mehWords, badWords, worstWords)
+        ps = len(partition_to_sc_com)
+        partition_sizes[ps-1] += 1
+        partition_scores[ps-1] += sc
+        partition_scores_max[ps-1] = max(sc, partition_scores_max[ps-1])
 
-    bestSoFar = [0, '', []]
-    for vocabWord in MODEL.vocab.keys():
-        sum = 0
-        relevant = []
-        for term in equation:
-            distance = getWordSimilarity(vocabWord, term[1])
-            if distance > RELEVANCE_THRESHOLD_LOW and distance < RELEVANCE_THRESHOLD_HIGH:
-                sum += distance * term[0]
-                relevant.append([term[1], distance, term[0]])
-        if sum > bestSoFar[0]:
-            bestSoFar = [sum, vocabWord, relevant]
+        if sc > best_score:
+          best_score = sc
+          best_partition = partition_to_sc_com
+
+    print(partition_sizes)
+    print(partition_scores_max)
+    print(partition_scores/partition_sizes)
+
+    #print(best_partition)
+    print(len(best_partition), sum(len(wset) for wset in best_partition))
+    first_wset = max(best_partition, key=lambda x: best_partition[x][0])
+    com = best_partition[first_wset][1]
+
+    target = find_closest(com, candidate_words, vectors, allWords)
+    bestSoFar = [best_partition[first_wset][0], target, first_wset]
     return bestSoFar
 
 def getMove2(team):
@@ -125,7 +191,7 @@ def getMove2(team):
                 sim = result[1] * math.pow(i, SIM_FACTOR)
                 if sim > bestCombo[0]:
                     bestCombo = [sim, result[0], combo]
-        print bestCombo
+        print(bestCombo)
     return bestCombo
 
 def prettyPrintBoard():
@@ -134,16 +200,16 @@ def prettyPrintBoard():
     c = 0
     for word in allWords:
         if c == sq:
-            print ''
+            print('')
             c = 0
-        print word, " " * (13 - len(word)),
+        print(word, " " * (13 - len(word)))
         c +=1
-    print ''
+    print('')
     for k in BOARD:
-        print "---", str(k), "---"
+        print ("---", str(k), "---")
         for word in BOARD[k]:
             print(word)
-    
+
 loadModel(500000)
 loadWords()
 fillBoard()
